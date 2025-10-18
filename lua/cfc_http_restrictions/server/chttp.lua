052380d9e4e144f157a6db2fde78125a1089389f
Tim Schumacher <timschumi@gmx.de>
2025-10-19 01:00:01 +0200

    Fix up CHTTP caller extraction

--- lua/cfc_http_restrictions/server/chttp.lua
+++ lua/cfc_http_restrictions/server/chttp.lua
@@ -1,3 +1,15 @@
+local function chttpCallerFromStack( stack )
+    -- CFunctions can't default to a proper funcname / namewhat, so we have to rely on the stack layout.
+    -- This should remain stable as long as hook.Run gets tail call optimized into hook.Call.
+    --
+    -- 1: "stack traceback"
+    -- 2: hook function
+    -- 3: hook.Call
+    -- 4: CHTTP
+    -- 5: caller
+    return stack[5]
+end
+
 hook.Add( "OnCHTTPRequest", "CFC_HTTP_CHTTP", function( req )
     if not req.url and not req.method then return end
 
@@ -9,7 +21,7 @@ hook.Add( "OnCHTTPRequest", "CFC_HTTP_CHTTP", function( req )
     CFCHTTP.LogRequest( {
         noisy = noisy,
         method = req.method,
-        fileLocation = getSourceFromStack( stack ),
+        fileLocation = chttpCallerFromStack( stack ),
         urls = { { url = req.url, status = isAllowed and "allowed" or "blocked" } },
     } )
 
